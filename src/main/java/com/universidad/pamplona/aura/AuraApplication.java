e11e331c6fe823584332927923ade37a13146a20
Jherson Sanchez <jhersonsanchez905@gmail.com>
2026-05-11 08:45:09 -0500

    chore: configurar proyecto Spring Boot con dependencias AURA

---
+++ src/main/java/com/universidad/pamplona/aura/AuraApplication.java
@@ -0,0 +1,13 @@
+package com.universidad.pamplona.aura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuraApplication.class, args);
+	}
+
+}
